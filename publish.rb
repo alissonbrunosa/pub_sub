9bea3221010b5b9f0cc5a6777b048602afd9cf2a
Alisson Bruno <alissonbruno.sa@gmail.com>
2014-12-14 14:21:22 -0300

    Alteração do arquivo publishr.rb, removi a pausa que tinha

--- publish.rb
+++ publish.rb
@@ -7,11 +7,8 @@ require_relative 'event'
 redis = Redis.new(host: 'localhost')
 
 loop do
-	sleep(0.01)
 	puts "Nova metrica registrada #{Time.now.strftime('%d/%m/%Y %H:%M:%S.%L')}"
 	redis.publish('process_metric', Metric.new(lat:100, lng:100).to_json)
-
-	sleep(0.01)
 
 	puts "Nova evento registrado #{Time.now.strftime('%d/%m/%Y %H:%M:%S.%L')}"
 	redis.publish('process_event', Event.new(:ERROR, "Erro no sistema do modulo").to_json)
