bf5145d51695baa8ccba2ba478d2f8a8295efa97
Alisson B. Santos <alissonbruno.sa@gmail.com>
2019-01-23 11:35:16 -0200

    Remove thread

--- subscriber.rb
+++ subscriber.rb
@@ -6,10 +6,8 @@ redis = Redis.new(host: "localhost")
 
 redis.subscribe("process_event", "process_metric") do |on|
   on.message do |channel, message|
-    Thread.new do 
-      executor = Executor.new	
-      executor.send(channel, message)
-    end
+    executor = Executor.new	
+    executor.send(channel, message)
   end
 end
 
